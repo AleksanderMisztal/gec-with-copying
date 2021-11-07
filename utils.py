7d2118b193effbaacf660ae70d5ab2689ebeab42
Aleksander Misztal <apm81@cam.ac.uk>
2021-11-07 15:45:13 +0000

    Removed some stale code

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import random
 import torch
 import json
 
@@ -11,22 +10,6 @@ def to_padded_tensor(xss, pad_token):
 	w = max(len(xs) for xs in xss)
 	padded = [xs + [pad_token for _ in range(w - len(xs))] for xs in xss]
 	return torch.tensor(padded)
-
-def noise(orig: 'list[int]', vocab_s: int):
-  x = orig.copy()
-  y = [0 for _ in x]
-  i = 0
-  while i < len(x)-1:
-    a = random.random()
-    if a < .15:
-      y[i] = y[i+1] = 1
-      x[i], x[i+1] = x[i+1], x[i]
-      i+=1
-    elif a < .3:
-      x[i] = random.randint(0, vocab_s-1)
-      y[i] = 2
-    i+=1
-  return x, y
 
 def count_params(model):
   return sum(p.numel() for p in model.parameters() if p.requires_grad)
